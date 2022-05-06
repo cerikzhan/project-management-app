136b9c934f02c50f8ea078cc1c6ac9455187b0cb
Наталья Панфиленко <np@plus.local>
2022-05-06 12:07:23 +0300

    feat: header buttons layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,20 +5,27 @@ import { Link } from 'react-router-dom';
 const Header: React.FC = () => (
   <header className={cl.header}>
     <div className="container">
-      <nav className={cl.header__navbar}>
-        <Link className={cl.header__navbar_link} to="/">
-          Home
-        </Link>
-        <Link className={cl.header__navbar_link} to="/login">
-          Login
-        </Link>
-        <Link className={cl.header__navbar_link} to="/projects">
-          Projects
-        </Link>
-        <Link className={cl.header__navbar_link} to="/projects/1">
-          Single Project
-        </Link>
-      </nav>
+      <div className={cl.header__container}>
+        <nav className={cl.header__navbar}>
+          <Link className={cl.header__navbar_link} to="/">
+            Home
+          </Link>
+          <Link className={cl.header__navbar_link} to="/login">
+            Login
+          </Link>
+          <Link className={cl.header__navbar_link} to="/projects">
+            Projects
+          </Link>
+          <Link className={cl.header__navbar_link} to="/projects/1">
+            Single Project
+          </Link>
+        </nav>
+        <div className={cl.header__usermenu}>
+          <div className={cl.header__usermenu_button}>Edit profile</div>
+          <div className={cl.header__usermenu_button}>Logout</div>
+          <div className={cl.header__usermenu_button}>New board</div>
+        </div>
+      </div>
     </div>
   </header>
 );

60b5ba813dab4d8459d08bb165587f94916cc835
Natalia Panfilenka <choco-cat@mail.u>
2022-05-09 19:41:55 +0300

    feat: sticky header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import cl from './header.module.scss';
 import { Link } from 'react-router-dom';
 import './../../assets/library/toggle.css';
 import Menu from './../Menu';
 
 const Header: React.FC = () => {
+  useEffect(() => {
+    window.addEventListener('scroll', isSticky);
+    return () => {
+      window.removeEventListener('scroll', isSticky);
+    };
+  });
+
+  const isSticky = () => {
+    const header = document.querySelector(`.${cl.header}`);
+    const scrollTop = window.scrollY;
+    if (header) {
+      scrollTop >= 250
+        ? header.classList.add(cl['header-sticky'])
+        : header.classList.remove(cl['header-sticky']);
+    }
+  };
+
   return (
     <header className={cl.header}>
       <div className="container">

48bd3bac66fc3c4a5072a41946f58276819353b0
Natalia Panfilenka <choco-cat@mail.u>
2022-05-09 21:29:01 +0300

    refactor: burger icon

--- src/components/Hamburger/index.tsx
+++ src/components/Hamburger/index.tsx
@@ -1,19 +1,21 @@
-//source https://webformyself.com/reaktivnoe-react-gamburger-menyu/
 import React from 'react';
-
-import { StyledHamburger } from './Hamburger.styled';
+import hb from './hamburger.module.scss';
 
 export type Props = {
   open: boolean;
   setOpen: (v: boolean) => void;
 };
 
-const Hamburger = (props: Props) => (
-  <StyledHamburger open={props.open} onClick={() => props.setOpen(!props.open)}>
-    <div />
-    <div />
-    <div />
-  </StyledHamburger>
-);
+const MyHamburger = (props: Props) => {
+  const classNameButton = props.open ? hb['hamburger-open'] : hb.hamburger;
+  const classNameLine = props.open ? hb['hamburger__line-open'] : hb.hamburger__line;
+  return (
+    <button className={classNameButton} onClick={() => props.setOpen(!props.open)}>
+      <div className={classNameLine}></div>
+      <div className={classNameLine}></div>
+      <div className={classNameLine}></div>
+    </button>
+  );
+};
 
-export default Hamburger;
+export default MyHamburger;

a60990cdb23459eacf30db06edfbb902c06b2d48
arteemm <arteemmarteemm@gmail.com>
2022-05-10 12:17:42 +0300

    feat: add footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,27 +3,45 @@ import cl from './footer.module.scss';
 
 const Footer: React.FC = () => (
   <footer className={cl.footer}>
-    <div className={cl.footer__container + ' container'}>
+    <div className={`${cl.footer__container} container`}>
       <div className={cl.footer__links}>
-        <a className="link" href="https://github.com/iamserik" target="_blank" rel="noreferrer">
+        <a
+          className={`${cl.footer__link} link`}
+          href="https://github.com/iamserik"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className={cl.footer__github} />
           iamserik
         </a>
-        <a className="link" href="https://github.com/choco-cat" target="_blank" rel="noreferrer">
+        <a
+          className={`${cl.footer__link} link`}
+          href="https://github.com/choco-cat"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className={cl.footer__github} />
           choco-cat
         </a>
-        <a className="link" href="https://github.com/arteemm" target="_blank" rel="noreferrer">
+        <a
+          className={`${cl.footer__link} link`}
+          href="https://github.com/arteemm"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className={cl.footer__github} />
           arteemm
         </a>
       </div>
-      <p className={cl.footer__year}>2022</p>
       <a
-        className={cl.footer__rss + ' link'}
+        className={`${cl.footer__rss} link`}
         href="https://rs.school/react/"
         target="_blank"
         rel="noreferrer"
       >
         <div className={cl.footer__logo}></div>
       </a>
+      <p className={cl.footer__year}>2022</p>
     </div>
   </footer>
 );

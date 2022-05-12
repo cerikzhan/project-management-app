981c4a6f7921569cc9b2077e44fa3000d2f69c1e
Natalia Panfilenka <choco-cat@mail.u>
2022-05-12 13:10:37 +0300

    feat: inject user data in profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useAppSelector } from '../hooks/redux';
+import { RootState } from '../store/store';
 
 const Profile: React.FC = () => {
+  const { user } = useAppSelector((state: RootState) => state.boards);
   const { t } = useTranslation();
-  return <div>{t('user.profile_page')}</div>;
+  return (
+    <>
+      {t('user.profile_page')}
+      console.log({JSON.stringify(user)});
+    </>
+  );
 };
 
 export default Profile;

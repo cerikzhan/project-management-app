85f6bc37af1e4f8f1670e77fa669591a6a7d57cf
Natalia Panfilenka <choco-cat@mail.u>
2022-05-17 11:19:28 +0300

    refactor: code refactoring

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,8 +13,8 @@ const Login: React.FC = () => {
   const dispatch = useAppDispatch();
   const handleLogin = async (event: React.FormEvent) => {
     event.preventDefault();
-    await dispatch(login({ login: username, password }));
-    await dispatch(authUser());
+    dispatch(login({ login: username, password }));
+    dispatch(authUser());
   };
 
   useEffect(() => {

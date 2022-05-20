54d0288cf93a8766f2a274a1d60a376fd886ff76
Natalia Panfilenka <choco-cat@mail.u>
2022-05-20 11:10:11 +0300

    feat: add language locales for column

--- src/components/Board/Column/index.tsx
+++ src/components/Board/Column/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Task } from '../Task';
 import { ColumnItem } from '../../../types/Entities/Column';
 import cl from './column.module.scss';
+import { useTranslation } from 'react-i18next';
 
 type ColumnProps = {
   column: ColumnItem;
 };
 
 export const Column: React.FC<ColumnProps> = ({ column }) => {
+  const { t } = useTranslation();
   return (
     <div className={cl.column}>
       <div className={cl.column__header}>
@@ -15,7 +17,7 @@ export const Column: React.FC<ColumnProps> = ({ column }) => {
         <button className={cl.column__add_task} title="Add task">
           +
         </button>
-        <button className="button_trash fa fa-trash-o"></button>
+        <button className="button_trash fa fa-trash-o" title={t('column.delete')}></button>
       </div>
       {column.tasks.map((task) => (
         <Task task={task} key={task.id} />
---
+++ src/locales/ru/column.ts
@@ -0,0 +1,3 @@
+export default {
+  delete: 'Удалить колонку',
+};
